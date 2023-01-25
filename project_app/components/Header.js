e32a781e6c94e2402c9138e862a91480280ba907
Murial Anindo <murial.anindo@lonett.com>
2023-01-25 10:06:31 -0500

    working on header component-added two sections

---
+++ project_app/components/Header.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h1>AIN'T BOARD</h1>
+        <button></button>
+        <button>Login</button>
+        <button>Join Us!</button>
+      </div>
+      <div className="links"></div>
+    </header>
+  );
+}

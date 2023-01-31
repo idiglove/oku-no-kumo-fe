e63393512fc7c0cfaf7e1eb37dfb197611090760
Murial Anindo <murial.anindo@lonett.com>
2023-01-30 21:09:27 -0500

    adding svg vectors

--- project_app/components/Header.js
+++ project_app/components/Header.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import SearchIcon from "@mui/icons-material/Search";
+import Vector from "../assets/Vector.svg";
 
 export default function Header() {
   return (
@@ -15,19 +16,16 @@ export default function Header() {
               marginRight: "60px",
             }}
           >
-            <input
-              placeholder="Search board game"
-              style={{ width: "400px", height: "30px" }}
-            />
+            <input placeholder="Search board game" />
             <SearchIcon />
-            <div className="buttons">
-              {" "}
-              <button>Events</button>
-              <button>FAQ</button>
-              <button>About Us</button>
-              <button>Game Night</button>
-            </div>
           </button>
+          <div className="buttonTypes">
+            <button>Events</button>
+            <button>FAQ</button>
+            <button>About Us</button>
+            <button>Game Night</button>
+          </div>
+
           <button style={{ marginRight: "30px" }}>Login</button>
           <button>Join Us!</button>
         </div>
@@ -43,6 +41,21 @@ const Container = styled.div`
     padding-left: 60px;
     text-transform: uppercase;
   }
+  input {
+    position: absolute;
+    width: 318px;
+    height: 31px;
+    font-family: "Quicksand";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 30px;
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.065em;
+    border-style: none;
+    color: #afa5a5;
+  }
   .buttons {
     display: flex;
     padding-left: 200px;
@@ -54,6 +67,18 @@ const Container = styled.div`
     line-height: 24px;
     letter-spacing: 0.025em;
     color: #ffffff;
+  }
+
+  .buttonTypes {
+    box-sizing: border-box;
+    position: absolute;
+    display: flex;
+    width: 124px;
+    height: 44px;
+    left: 387px;
+    top: 217px;
+    border: 2px solid #ffffff;
+    border-radius: 10px;
   }
   section {
     width: 1440px;
@@ -67,16 +92,5 @@ const Container = styled.div`
     height: 47px;
     background: #ffffff;
     border-radius: 10px;
-  }
-  h3 {
-    font-family: "Quicksand";
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 30px;
-    display: inline-block;
-    letter-spacing: 0.025em;
-    color: #ffffff;
-    padding: 30px;
   }
 `;

02d24ee7ce3bc8368afd6f2a7a94fa84ee0dd4d8
Murial Anindo <murial.anindo@lonett.com>
2023-02-07 12:26:53 -0500

    added styled.ts file

--- project_app/components/Card.js
+++ project_app/components/Card.js
@@ -2,7 +2,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Image from "next/image";
-import styled from "styled-components";
+import { Card, Typography } from "./styled";
 
 function CardGame() {
   return (
@@ -35,31 +35,5 @@ function CardGame() {
     </Card>
   );
 }
-
-const Typography = styled.text`
-  width: 152px;
-  height: 86px;
-  left: 34px;
-  top: 169px;
-  text-align: center;
-  font-family: "Rubik";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #000000;
-`;
-
-const Card = styled.div`
-  flexdirection: column;
-  width: 220px;
-  height: 380px;
-  border: 2px solid #000000;
-  borderradius: 10px;
-  boxsizing: border-box;
-  left: 0px;
-  top: 0px;
-  background: white;
-`;
 
 export default CardGame;

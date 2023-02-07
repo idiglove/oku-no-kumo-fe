529dd0ded8358986e8cc7c11afd3b41c16986ad1
Murial Anindo <murial.anindo@lonett.com>
2023-02-07 18:58:48 -0500

    finally getting all the pieces

--- project_app/components/Header.js
+++ project_app/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
-import styled from "styled-components";
 import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import { Container } from "./styled";
 import Image from "next/image";
 
 function Header() {
@@ -11,7 +11,7 @@ function Header() {
     <>
       <AppBar
         style={{
-          height: "412px",
+          height: "432px",
           backgroundColor: "#353535",
           width: "1440px",
           position: "absolute",
@@ -28,103 +28,53 @@ function Header() {
               spacing={4}
               style={{
                 padding: "140px",
-                borderRadius: "100px",
+                borderRadius: "200px",
+                marginLeft: "400px",
               }}
             >
               <Chip label="Events" variant="outlined" />
               <Chip label="FAQ" variant="outlined" />
               <Chip label="About Us" variant="outlined" />
               <Chip label="Game night" variant="outlined" />
-              <div>
-                <h3>Interactive BoardGame Community</h3>
-              </div>
             </Stack>
-
-            <Image src="/Vector.jpg" width={520} height={280} alt="image" />
-            <Image src="/trees.jpg" width={520} height={280} alt="image" />
+            <main
+              className="images"
+              style={{
+                backgroundColor: "#353535",
+                height: "432px",
+                width: "1440px",
+                position: "absolute",
+              }}
+            >
+              <h3
+                style={{
+                  padding: "40px",
+                  width: "521px",
+                  height: "86px",
+                  left: "486px",
+                  top: "300px",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  fontSize: "30px",
+                  lineHeight: "43px",
+                  textTransform: "uppercase",
+                  color: "#ffffff",
+                }}
+              >
+                Interactive BoardGame Community
+              </h3>
+              <Image src="/Vector.jpg" width={300} height={400} alt="image" />
+              <Image src="/trees.jpg" width={300} height={400} alt="image" />
+              <Image src="/tent3.jpg" width={100} height={200} alt="image" />
+              <Image src="/tent.jpg" width={100} height={200} alt="image" />
+              <Image src="/Vector.jpg" width={200} height={380} alt="image" />
+              <Image src="/trees.jpg" width={300} height={380} alt="image" />
+            </main>
           </section>
         </Container>
       </AppBar>
     </>
   );
 }
-
-const Container = styled.div`
-  h1 {
-    position: absolute;
-    width: 227.77px;
-    height: 34.34px;
-    left: 50px;
-    top: 50px;
-    color: white;
-  }
-  .chip {
-    padding-left: 180px;
-    height: 44px;
-    border-radius: 50px;
-    left: 387px;
-  }
-  section {
-    color: white;
-    text-align: center;
-    align: center;
-  }
-  input {
-    box-sizing: border-box;
-    position: absolute;
-    width: 614px;
-    height: 67px;
-    left: 413px;
-    top: 137px;
-    backgroundcolor: #ffffff;
-  }
-  Button {
-    width: 182px;
-    margin: 10px;
-    height: 47px;
-    left: 500px;
-    top: 50px;
-    background: #ffffff;
-    border-radius: 10px;
-    color: black;
-  }
-  Box {
-    box-sizing: border-box;
-    position: absolute;
-    width: 84px;
-    height: 22px;
-    left: 387px;
-    top: 217px;
-    border: 2px solid #ffffff;
-    border-radius: 10px;
-    backgroundcolor: blue;
-  }
-
-  Stack {
-    box-sizing: border-box;
-    position: absolute;
-    width: 124px;
-    height: 34px;
-    left: 387px;
-    top: 100px;
-
-    border: 2px solid #ffffff;
-  }
-  h3 {
-    padding: 40px;
-    width: 521px;
-    height: 86px;
-    left: 486px;
-    top: 300px;
-    font-family: "Rubik";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 30px;
-    line-height: 43px;
-    text-align: center;
-    text-transform: uppercase;
-    color: #ffffff;
-  }
-`;
 
 export default Header;
---
+++ project_app/components/styled.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  h1 {
+    position: absolute;
+    width: 227.77px;
+    height: 34.34px;
+    left: 50px;
+    top: 50px;
+    color: white;
+  }
+  section {
+    color: white;
+    text-align: center;
+    align: center;
+  }
+  input {
+    box-sizing: border-box;
+    position: absolute;
+    width: 614px;
+    height: 67px;
+    left: 413px;
+    top: 137px;
+    backgroundcolor: #ffffff;
+  }
+  Button {
+    width: 182px;
+    margin: 10px;
+    height: 47px;
+    left: 500px;
+    top: 50px;
+    background: #ffffff;
+    border-radius: 10px;
+    color: black;
+  }
+  Box {
+    box-sizing: border-box;
+    position: absolute;
+    width: 84px;
+    height: 22px;
+    left: 387px;
+    top: 217px;
+    border: 2px solid #ffffff;
+    border-radius: 10px;
+    backgroundcolor: blue;
+  }
+
+  Stack {
+    box-sizing: border-box;
+    position: absolute;
+    width: 124px;
+    height: 34px;
+    left: 387px;
+    top: 100px;
+    border: 2px solid #ffffff;
+  }
+  main{
+    backgroundColor: #353535,
+  }
+ h3{
+  padding: 40px,
+  width: 521px,
+  height: 86px,
+align-items:center,
+left: 486px,
+top: 300px,
+font-style: normal,
+font-weight: 700,
+font-size: 30px,
+line-height: 43px,
+text-transform: uppercase,
+color: #ffffff,
+ }
+  
+`;

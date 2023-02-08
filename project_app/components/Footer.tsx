55377846b87797a3a5a70dc1a05cacc7db40585e
Murial Anindo <murial.anindo@lonett.com>
2023-02-08 15:27:29 -0500

    minor tweak

--- project_app/components/Footer.tsx
+++ project_app/components/Footer.tsx
@@ -11,8 +11,8 @@ export default function Footer() {
         <Text>Give Feedback</Text>
       </div>
       
-      <div style={{paddingTop:"30px"}}>
-         <Image src="/bush.jpg" alt="love" width={238.2} height={106.1} style={{marginRight:"960px"}}/>
+      <div style={{paddingTop:"20px"}}>
+         <Image src="/bush.jpg" alt="love" width={238.2} height={106.1} style={{marginRight:"962px"}}/>
           <Image src="/bush2.jpg" alt="love" width={238.2} height={106.1} />
         <Image src="/footer.jpg" alt="love" width={1440} height={43.6} />
         <Image src="/footer2.jpg" alt="love" width={1440} height={43.6} />

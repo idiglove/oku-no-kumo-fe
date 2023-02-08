cb34d6aade9b90f4e55195eff8bc3ce07b9ec9d5
Murial Anindo <murial.anindo@lonett.com>
2023-02-08 15:21:23 -0500

    adding assets from figma

---
+++ project_app/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { Box, Text } from "./styled";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <Box>
+      <div>
+        <Text>About Us</Text>
+        <Text>FAQ</Text>
+        <Text>Contact Us</Text>
+        <Text>Give Feedback</Text>
+      </div>
+      
+      <div style={{paddingTop:"30px"}}>
+         <Image src="/bush.jpg" alt="love" width={238.2} height={106.1} style={{marginRight:"960px"}}/>
+          <Image src="/bush2.jpg" alt="love" width={238.2} height={106.1} />
+        <Image src="/footer.jpg" alt="love" width={1440} height={43.6} />
+        <Image src="/footer2.jpg" alt="love" width={1440} height={43.6} />
+      </div>
+    </Box>
+  );
+}
---
+++ project_app/components/styled.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  position: absolute;
+  width: 1440px;
+  height: 379px;
+  left: -4px;
+  top: 2712px;
+  background: #353535;
+`;
+
+export const Text = styled.div`
+padding:20px;
+padding-top:20px
+    width: 92px;
+    height: 24px;
+    left: 117px;
+    top: 2777px;
+    font-family: "Rubik";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    letter-spacing: 0.025em;
+    color: #ffffff;
+  }
+`;

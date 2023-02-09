4001d36d680bce894106b1e12cc03ba4b93cfae6
Murial Anindo <murial.anindo@lonett.com>
2023-02-08 21:37:54 -0500

    more styling changes

--- project_app/components/Header.js
+++ project_app/components/Header.js
@@ -1,22 +1,14 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import { Container } from "./styled";
+import { Container, AppBar, Grid, Wrapper, Text } from "./styled";
 import Image from "next/image";
 
 function Header() {
   return (
     <>
-      <AppBar
-        style={{
-          height: "432px",
-          backgroundColor: "#353535",
-          width: "1440px",
-          position: "absolute",
-        }}
-      >
+      <AppBar>
         <Container maxWidth="xl">
           <h1>AIN'T BOARD</h1>
           <section>
@@ -37,39 +29,59 @@ function Header() {
               <Chip label="About Us" variant="outlined" />
               <Chip label="Game night" variant="outlined" />
             </Stack>
-            <main
-              className="images"
-              style={{
-                backgroundColor: "#353535",
-                height: "432px",
-                width: "1440px",
-                position: "absolute",
-              }}
-            >
-              <h3
+
+            <Grid>
+              <Text> Interactive BoardGame Community</Text>
+              <main
+                className="images"
                 style={{
-                  padding: "40px",
-                  width: "521px",
-                  height: "86px",
-                  left: "486px",
-                  top: "300px",
-                  fontStyle: "normal",
-                  fontWeight: 700,
-                  fontSize: "30px",
-                  lineHeight: "43px",
-                  textTransform: "uppercase",
-                  color: "#ffffff",
+                  height: "432px",
+                  width: "1440px",
+                  position: "absolute",
+                  background: "#353535",
                 }}
               >
-                Interactive BoardGame Community
-              </h3>
-              <Image src="/Vector.jpg" width={300} height={400} alt="image" />
-              <Image src="/trees.jpg" width={300} height={400} alt="image" />
-              <Image src="/tent3.jpg" width={100} height={200} alt="image" />
-              <Image src="/tent.jpg" width={100} height={200} alt="image" />
-              <Image src="/Vector.jpg" width={200} height={380} alt="image" />
-              <Image src="/trees.jpg" width={300} height={380} alt="image" />
-            </main>
+                <Wrapper>
+                  <Image
+                    src="/Vector.jpg"
+                    width={300}
+                    height={400}
+                    alt="image"
+                  />
+                  <Image
+                    src="/trees.jpg"
+                    width={200}
+                    height={463}
+                    alt="image"
+                  />
+                  <Image
+                    src="/tent3.jpg"
+                    width={100}
+                    height={200}
+                    alt="image"
+                  />
+                  <Image src="/tent.jpg" width={100} height={200} alt="image" />
+                  <Image
+                    src="/trees.jpg"
+                    width={274.18}
+                    height={422.36}
+                    alt="image"
+                  />
+                  <Image
+                    src="/Vector.jpg"
+                    width={200}
+                    height={380}
+                    alt="image"
+                  />
+                  <Image
+                    src="/trees.jpg"
+                    width={200}
+                    height={380}
+                    alt="image"
+                  />
+                </Wrapper>
+              </main>
+            </Grid>
           </section>
         </Container>
       </AppBar>
--- project_app/components/styled.tsx
+++ project_app/components/styled.tsx
@@ -2,13 +2,24 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   h1 {
-    position: absolute;
-    width: 227.77px;
-    height: 34.34px;
-    left: 50px;
-    top: 50px;
-    color: white;
+position: absolute;
+width: 227.77px;
+height: 34.34px;
+left: 50px;
+top: 50px;
+background: #FFFFFF;
   }
+  Button {
+    width: 182px;
+    margin: 10px;
+    height: 47px;
+    left: 500px;
+    top: 50px;
+    background: #ffffff;
+    border-radius: 10px;
+    color: black;
+  }
+
   section {
     color: white;
     text-align: center;
@@ -22,54 +33,74 @@ export const Container = styled.div`
     left: 413px;
     top: 137px;
     backgroundcolor: #ffffff;
-  }
-  Button {
-    width: 182px;
-    margin: 10px;
-    height: 47px;
-    left: 500px;
-    top: 50px;
-    background: #ffffff;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
     border-radius: 10px;
-    color: black;
-  }
-  Box {
-    box-sizing: border-box;
-    position: absolute;
-    width: 84px;
-    height: 22px;
-    left: 387px;
-    top: 217px;
-    border: 2px solid #ffffff;
-    border-radius: 10px;
-    backgroundcolor: blue;
   }
 
-  Stack {
+`;
+
+export const AppBar = styled.div`
+position: absolute;
+width: 1440px;
+height: 412px;
+left: 0px;
+top: 0px;
+background: #353535;
+`
+export const Box = styled.div`
+box-sizing: border-box;
+position: absolute;
+width: 124px;
+height: 44px;
+left: 387px;
+top: 217px;
+border: 2px solid #FFFFFF;
+border-radius: 10px;
+`
+export const Grid = styled.div`
+width: 1440px;
+height: 402px;
+left: 0px;
+top: 0px;
+background: #353535;
+`
+
+export const Stack = styled.div`
     box-sizing: border-box;
     position: absolute;
     width: 124px;
-    height: 34px;
+    height: 44px;
     left: 387px;
     top: 100px;
     border: 2px solid #ffffff;
-  }
-  main{
-    backgroundColor: #353535,
-  }
- h3{
-  padding: 40px,
-  width: 521px,
-  height: 86px,
-align-items:center,
-left: 486px,
-top: 300px,
-font-style: normal,
-font-weight: 700,
-font-size: 30px,
-line-height: 43px,
-text-transform: uppercase,
-color: #ffffff,
- }
-  
-`;
+    padding: 140px,
+    borderRadius: 200px,
+    marginLeft: 400px,
+
+`
+export const Wrapper= styled.div`
+position: absolute;
+width: 1440px;
+height: 312px;
+left: 0px;
+top: 0px;
+background: #353535;
+
+`
+
+export const Text = styled.p`
+display:flex;
+width: 521px;
+height: 86px;
+left: 486px;
+top: 368px;
+font-family: 'Rubik';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+line-height: 43px;
+text-align: center;
+text-transform: uppercase;
+color: #FFFFFF;
+`

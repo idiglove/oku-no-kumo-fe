603c23477fc28d76275d1bb292f8df390c9057fb
Murial Anindo <murial.anindo@lonett.com>
2023-02-21 18:44:28 -0500

    adding the cards on the reviews page

---
+++ project_app/components/CardContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CardContent() {
+  return <div>CardContent</div>;
+}
+
+export default CardContent;
--- project_app/components/ReviewCard.js
+++ project_app/components/ReviewCard.js
@@ -3,10 +3,30 @@ import { Paper, Box } from "./styled";
 export default function ReviewCard() {
   return (
     <Box>
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
+      <div>
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </div>
+      <div>
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </div>
+      <div>
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </div>
+      <div>
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </div>
     </Box>
   );
 }

9c5aabd14d54a858ac73d2a98a1e3f9dba0e55fc
Murial Anindo <murial.anindo@lonett.com>
2023-03-09 12:22:40 -0500

    moving to styled components

--- project_app/components/ToolBar.js
+++ project_app/components/ToolBar.js
@@ -1,46 +1,23 @@
-import styled from "styled-components";
 import Image from "next/image";
+import { StyledContainer } from "./styled";
 
 function ToolBar() {
   return (
-    <Container>
-      <div className="search">
-        <div>
-          <input type="text" placeholder="Search board game" />
-          <div
-            style={{
-              position: "absolute",
-              width: "28.99px",
-              height: "28.73px",
-              top: "168px",
-              left: "960px",
-            }}
-          >
-            <Image src="/search.png" fill="absolute" alt="image" />
-          </div>
-        </div>
-      </div>
-    </Container>
+    <div>
+      <StyledContainer>
+        <input type="text" placeholder="Search board game" />
+        <section
+          style={{
+            top: "170px",
+            left: "970px",
+            position: "absolute",
+          }}
+        >
+          <Image src="/search.png" width={18.99} height={18.73} alt="image" />
+        </section>
+      </StyledContainer>
+    </div>
   );
 }
-
-const Container = styled.div`
-  input {
-    box-sizing: border-box;
-    position: absolute;
-    width: 614px;
-    height: 67px;
-    left: 400px;
-    top: 150px;
-    color: black;
-    background: #ffffff;
-    border: 1px solid rgba(0, 0, 0, 0.05);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-    border-radius: 10px;
-    font-size: 1.2rem;
-    font-family: "Quicksand", sans-serif;
-    padding-left: 40px;
-  }
-`;
 
 export default ToolBar;

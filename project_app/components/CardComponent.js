0180963093a63d50115a1bd730b8ad55b674cb4d
Murial Anindo <murial.anindo@lonett.com>
2023-03-10 10:13:10 -0500

    making more commits

--- project_app/components/CardComponent.js
+++ project_app/components/CardComponent.js
@@ -8,20 +8,25 @@ function CardComponent() {
   return (
     <div>
       <Card>
-        <Image src="/background.png" width={216} height={105} alt="image" />
+        <div>
+          <Image src="/background.png" width={216} height={105} alt="image" />
+        </div>
+
         <CardContent>
           <Typography>
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua...."
           </Typography>
         </CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Heading
-        </Typography>
         <CardActions>
           <Image src="/Dice.png" width={35} height={35} alt="image" />
-          <Image src="/chat.png" width={12} height={12} alt="image" />
-          <Image src="/love.png" width={12} height={12} alt="image" />
+          <div>
+            <Typography gutterBottom variant="h5" component="h2">
+              Heading
+            </Typography>
+            <Image src="/chat.png" width={12} height={12} alt="image" />
+            <Image src="/love.png" width={12} height={12} alt="image" />
+          </div>
         </CardActions>
       </Card>
     </div>

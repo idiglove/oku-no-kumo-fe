0d174b9c5ee11e5c73a4cf74eacbf4d6e4fd2539
Murial Anindo <murial.anindo@lonett.com>
2023-03-10 12:21:27 -0500

    styling changes

--- project_app/components/styled.tsx
+++ project_app/components/styled.tsx
@@ -2,21 +2,10 @@ import styled from "styled-components";
 
 
 
-export const Paper = styled.div`
-box-sizing: border-box;
-width: 220px;
-height: 330px;
-left: 0px;
-top: 0px;
-margin:30px;
-background: white;
-border: 2px solid #000000;
-border-radius: 10px;
-`;
-
 export const Box = styled.div`
 display:flex;
 padding:107px;
+
 `;
 
 

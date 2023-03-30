a3c1b768caeb71e631165d72c7a1e96e0d646ee0
Murial Anindo <murial.anindo@lonett.com>
2023-03-30 13:27:44 -0400

    working on About page

--- project_app/pages/About.tsx
+++ project_app/pages/About.tsx
@@ -1,9 +1,82 @@
+import Footer from "../components/Footer";
+import NavBarButtons from "../components/NavBarButtons";
+import ToolBar from "../components/ToolBar";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import styled from "styled-components";
 
-
-function about() {
+function About() {
   return (
-    <div>About</div>
+    <Container>
+    <HeaderContainer>
+            <Image
+              src="/logo.svg"
+              width={227.77}
+              height={150}
+              alt="Ain't Board Logo"
+            />
+            <HeaderButtons>
+ <Button>Login</Button>
+              <Button>
+                Join Us!
+              </Button>
+            </HeaderButtons>
+              
+       <ul>
+        <li>Events</li>
+        <li>FAQ</li>
+        <li>About Us</li>
+       </ul>
+          <ToolBar/>
+      
+      </HeaderContainer>
+      
+        <FooterContainer>
+         
+        </FooterContainer>
+
+    </Container>
+   
   )
 }
 
-export default about
+export default About
+const Container= styled.div`
+width: 1440px;
+height: 1467px; 
+
+`
+const FooterContainer=styled.div`
+
+
+`
+
+const HeaderContainer=styled.div`
+
+position: absolute;
+width: 1440px;
+height: 312px;
+left: 0px;
+top: 0px;
+
+background: #353535;
+
+`
+const HeaderButtons=styled.button`
+width: 162px;
+height: 47px;
+left: 1046px;
+top: 50px;
+font-family: 'Rubik';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 24px;
+display: flex;
+align-items: center;
+text-align: center;
+letter-spacing: 0.025em;
+color: #000000;
+
+`
+

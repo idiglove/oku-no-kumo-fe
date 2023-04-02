6253d3db5ccebbc1f4af3a11bff69dd57965c1ce
Murial Anindo <murial.anindo@lonett.com>
2023-04-02 14:44:02 -0400

    almost done with the header part

--- project_app/pages/About.tsx
+++ project_app/pages/About.tsx
@@ -1,43 +1,77 @@
-import Footer from "../components/Footer";
-import ToolBar from "../components/ToolBar";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import Wrapper from "@mui/material/Button";
+import NavBarButtons from "../components/NavBarButtons";
 import Image from "next/image";
 import styled from "styled-components";
+import Footer from "../components/Footer";
 
 function About() {
+  
   return (
     <Container>
-    <HeaderContainer>
+      <div style={{
+    backgroundColor:"rgb(53, 53, 53)",
+    minHeight: "35rem",
+    }}>
+         <NavBarWrapper>
+        <HeaderContainer>
             <Image
               src="/logo.svg"
               width={227.77}
               height={150}
               alt="Ain't Board Logo"
             />
-            <HeaderButtons>
-             <Button>Login</Button>
-              <Button>
-                Join Us!
-              </Button>
-            </HeaderButtons>
-             <NavLinks>
-                <ul>
-        <li>Events</li>
-        <li>FAQ</li>
-        <li>About Us</li>
-       </ul>
-              </NavLinks> 
-    
-          <ToolBar/>
-      
+            <NavBarButtons/>
+           <div style={{display:"flex",flex: "1 1 0%",justifyContent:"flex-end",margin:"30px"}}>
+         <Button style={{padding:"10px"}}>Log In </Button>
+         <Button>Join Us </Button>
+           </div>
       </HeaderContainer>
-      <Box style={{height:"508.49px",width:"1440px",background:"#353535"}}>
-
-      </Box>
-     <FooterContainer>
-        <Footer/>
-     </FooterContainer>
+      <div
+            style={{
+              position: "absolute",
+              width: "629.05px",
+              height: "400.51px",
+              top: "162px",
+              left: "0",
+              bottom: "0",
+            }}
+          >
+            <Image src="/trees_left.png" layout="fill" alt="image" />
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              width: "296px",
+              height: "200px",
+              left: "412.31px",
+              top: "360px",
+            }}
+          >
+            <Image src="/Tent.png" layout="fill" alt="image" />
+          </div>
+             <div>
+            <StyledText>About Us</StyledText>
+            <p style={{color:"white",zIndex:"10",textAlign:"center"}}>Be a part of the best boardgame community.
+Make reviews and strategies. Join challenges and earn powerups to get new avatars.</p>
+      </div>
+           <div
+            style={{
+              position: "absolute",
+             
+              height: "400.65px",
+              left: "789.04px",
+              bottom: "0",
+              top: "162.54px",
+              right: "0",
+              zIndex: "100",
+            }}
+          >
+            <Image src="/trees_right.png" layout="fill" alt="image" />
+          </div>
+      </NavBarWrapper>
+       
+      </div>
     </Container>
    
   )
@@ -45,64 +79,33 @@ function About() {
 
 export default About
 const Container= styled.div`
-width: 1440px;
-height: 1467px; 
-
+width: 100%;
+display:block;
+box-sizing:border-box;
+height: 1172px; 
 `
-const FooterContainer=styled.div`
-position:absolute;
-width: 1440px;
-height: 379px;
-left: 0px;
-top: 1088px;
-background: #353535;
-
-`
-
 const HeaderContainer=styled.div`
-
-position: absolute;
-width: 1440px;
-height: 312px;
-left: 0px;
-top: 0px;
-
-background: #353535;
-
-`
-const HeaderButtons=styled.button`
-width: 162px;
-height: 47px;
-margin-left:1200px;
-font-family: 'Rubik';
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 24px;
 display: flex;
+justify-content: space-between;
 align-items: center;
+background-color: rgb(53, 53, 53);
+height: 8rem;
+position: static;
+width: 100%;
+
+`
+const NavBarWrapper=styled.div`
+display: block;
+height: 8rem;
+width:100%;
+`
+
+const StyledText= styled.h1`
+color: rgb(255, 255, 255);
+font-family: Rubik-Bold;
+font-size: 1.75rem;
+text-transform: uppercase;
+position: relative;
 text-align: center;
-letter-spacing: 0.025em;
-color: #000000;
+margin-top: 5rem;
 `
-
-const NavLinks=styled.div`
-ul{
-display: inline-block;
-color:white;
-position: absolute;
-width: 92px;
-height: 24px;
-left: 851px;
-top: 227px;
-font-family: 'Rubik';
-font-style: normal;
-font-weight: 500;
-font-size: 20px;
-line-height: 24px;
-letter-spacing: 0.025em;
-}
-
-
-`
-

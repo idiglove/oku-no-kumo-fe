9fe3537e8e030ed3745044e1f36782cc637d6a2e
Murial Anindo <murial.anindo@lonett.com>
2023-04-04 12:02:17 -0400

    adding more logic

--- project_app/pages/About.tsx
+++ project_app/pages/About.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import styled from "styled-components";
 
 
+
 function About() {
   
   return (
@@ -11,6 +12,7 @@ function About() {
       <div style={{
     backgroundColor:"rgb(53, 53, 53)",
     minHeight: "35rem",
+    width:"100%",
     }}>
          <NavBarWrapper>
         <HeaderContainer>
@@ -22,59 +24,55 @@ function About() {
             />
             <NavBarButtons/>
            <div style={{display:"flex",flex: "1 1 0%",justifyContent:"flex-end",margin:"30px"}}>
-         <Button style={{padding:"10px",width:"147px"}}>Log In </Button>
-         <Button>Join Us </Button>
+         <Button style={{padding:"10px",width:"147px", backgroundColor:"white",borderRadius:"10px", color:"black"}}>Log In </Button>
+         <Button style={{padding:"10px",width:"147px", backgroundColor:"white",borderRadius:"10px", color:"black"}}>Join Us </Button>
            </div>
       </HeaderContainer>
       <div
             style={{
               position: "absolute",
-              width: "435.05px",
-              height: "400.51px",
               top: "162px",
               left: "0",
               bottom: "0",
             }}
           >
-            <Image src="/trees_left.png" layout="fill" alt="image" />
+            <Image src="/trees_left.png" width={435}  height={400}alt="image" />
           </div>
           <div
             style={{
               position: "absolute",
-              width: "267px",
-              height: "198px",
               left: "412.31px",
               top: "360px",
             }}
           >
-            <Image src="/Tent.png" layout="fill" alt="image" />
+            <Image src="/Tent.png" width={267} height={198}alt="image" />
           </div>
              <div>
             <StyledText>About Us</StyledText>
-            <p style={{color:"white",zIndex:"100",textAlign:"center",position:"absolute",padding:"10px"}}>Be a part of the best boardgame community.
-<br/>Make reviews and strategies. Join challenges and earn powerups to get new avatars.</p>
-      </div>
+            <p style={{color:"white",zIndex:"100",textAlign:"center",padding:"10px"}}>Be a part of the best boardgame community.
+            Make reviews and strategies. Join challenges and earn powerups to get new avatars.</p>
+          </div>
            <div
             style={{
             position:"absolute",
-              width: "451px",
-              height: "400.65px",
-              left: "1014px",
-              bottom: "0",
-              top: "162.54px",
+            left: "1014px",
+            bottom: "0",
+            top: "162.54px",
               
-             
             }}
           >
-            <Image src="/trees_right.png" layout="fill" alt="image" />
+            <Image src="/trees_right.png" width={450} height={400}alt="image" />
           </div>
       </NavBarWrapper>
       </div>
+    
       <Wrapper>
-        <p>Were an interactive community of avid boardgamers - be it in sharing Reviews,<br/> participating and competing in Online Battles, achieving Challenges, and more. We want you to play more of your favorite board games with friends, family and even potential friends! We are in a very early stage of.....</p>
+        <p>We're an interactive community of avid boardgamers - be it in sharing Reviews, participating and competing in Online Battles, achieving Challenges, and more.<br></br> We want you to play more of your favorite board games with friends, family and even potential friends!<br></br>We are in a very early stage of development, thus bugs and hiccups are expected. Because of this, any feedback is welcome to make this place the best! So please contact us to give any feedback here.</p>
       </Wrapper>
      
-   
+  <FooterContainer>
+<h1>hello </h1>
+  </FooterContainer>
     </Container>
    
   )
@@ -114,14 +112,28 @@ margin-top: 5rem;
 `
 
 const Wrapper= styled.div`
-      background-color: rgb(53, 53, 53);
-    font-family: Quicksand-Regular;
-   
-    color: white;
-    height:212px;
-    position:absolute;
-    padding: 0px 2rem 2rem;
-    text-align: center;
-    display:block;
-    box-sizing:border-box;
+p{
+margin: 0px auto;
+width: 50%;
+display: block;
+text-align: center;
+font-family: Quicksand-Regular;
+font-size: 16px;
+left: 330px;
+top:100px;
+}
+background-color: rgb(53, 53, 53);
+font-family: Quicksand-Regular;
+color: white;
+height:212px;
+position:absolute;
+padding: 0px 2rem 2rem;
+text-align: center;
+width:100%;
+box-sizing:border-box;
+`
+const FooterContainer=styled.div`
+display: block;
+width:100%;
+background-color:green;
 `

503794fc4181b5bd65273e61bdcac37df49fb92c
Enrico Emil Dela Rosa <enricodelarosa01@gmail.com>
2023-04-07 22:17:11 +0800

    Fix font use for admin login page

--- project_app/components/AdminLayout.js
+++ project_app/components/AdminLayout.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Image from "next/image";
 import styled from "styled-components";
+import Head from 'next/head';
 
 export default function AdminLayout(props) {
-  return (
-    <AdminLayoutStyle>
-        <LogoContainer>
-        <Image
-          src="/logo.svg"
-          width={227.77}
-          height={150}
-          alt="Ain't Board Logo"
-        />
-        </LogoContainer>
-        <Content>{props.children}</Content>
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet" />
+            </Head>
+            <AdminLayoutStyle>
+                <LogoContainer>
+                    <Image
+                        src="/logo.svg"
+                        width={227.77}
+                        height={150}
+                        alt="Ain't Board Logo"
+                    />
+                </LogoContainer>
+                <Content>{props.children}</Content>
 
-    </AdminLayoutStyle>
-  );
+            </AdminLayoutStyle>
+        </>
+    );
 }
 
 const ImageContainer = styled.div`
@@ -27,7 +35,7 @@ const ImageContainer = styled.div`
   background-color: #353535;
 `;
 
-const LogoContainer =styled.div`
+const LogoContainer = styled.div`
   position: absolute;
   top: 0;
 `

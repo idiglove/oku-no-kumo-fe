645e9d1c3cede75cdba67c66a1663b0691d91a48
Murial Anindo <murial.anindo@lonett.com>
2023-05-02 15:09:57 -0400

    removed the hydration errors- it was due to the h1 inside of the h1

--- project_app/components/Header.js
+++ project_app/components/Header.js
@@ -35,7 +35,8 @@ function Header(props) {
             </div>
           </Container>
         </NavBarWrapper>
-        <Typography>{props.header}</Typography>
+        {/* <Typography>{props.header}</Typography> */}
+        {props.header}
       </AppBar>
     </>
   );

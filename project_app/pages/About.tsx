dbdf7b25c81027e0354f121467dd93e26bc4071e
Murial Anindo <murial.anindo@lonett.com>
2023-05-07 18:54:31 -0400

    new changes

--- project_app/pages/About.tsx
+++ project_app/pages/About.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Header from "../components/Header";
 import{NavbarText} from "./style";
 import styled from "styled-components";
+import {FooterBox} from "../components/styled";
 import Image from "next/image";
 
 
  
  function About() {
    return (
-    <>
+  
      <AboutContainer>
    <HeaderWrapper>
       <Header header={<NavbarText>About Us</NavbarText>}/> 
    </HeaderWrapper>
    <AboutWrapper>
-      <div
+      {/* <div
         style={{
           position: "absolute",
           width: "25rem",
@@ -34,7 +35,7 @@ import Image from "next/image";
         }}
       >
         <Image src="/trees_right.png" width={651} height={466} alt="imageNext" />
-      </div>
+      </div> */}
        <p>
         Were an interactive community of avid boardgamers - be it in sharing Reviews, participating and competing in Online Battles, achieving Challenges, and more.
         <br>
@@ -48,22 +49,31 @@ import Image from "next/image";
       </br>
     </p>
    </AboutWrapper>
+  
+<FooterBox/>
+  
   </AboutContainer>
-    </>
+    
    )
  }
 
 const AboutContainer=styled.div`
-width: 1440px;
-height: 1200px;
+width: 100%;
+    padding: 0px;
+    margin: 0px;
+    font-family: Rubik-Regular, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    font-size: 16px;
+
+height: 1112px;
 box-sizing: border-box;
 display:box;
-font-family: Rubik-Regular, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
 font-size: 16px;
  `
 const HeaderWrapper=styled.div`
 width: 100%;
 background-color: rgb(53, 53, 53);
+font-family: Rubik-Bold;
+font-size: 1.75rem;
 height:557px;
 
  `
@@ -76,18 +86,27 @@ text-align: center;
 
 p{
 position: absolute;
+margin: 0px auto;
+width: 50%;
+display: block;
 width: 1266px;
 height: 1017px;
 left: 91px;
 top: 714px;
 font-family: 'Quicksand';
-font-style: normal;
-font-weight: 700;
-font-size: 24px;
+/* font-style: normal;
+font-weight: 700; */
+font-size: 16px;
 line-height: 30px;
 color: #FFFFFF;
 }
 
+ `
+
+const FooterContainer=styled.div`
+background-color:pink;
+height:400px;
+width:100%;
  `
 
  export default About;

3ef9c5230a8e4f780ac06b68af75d9f8ce150ef4
Murial Anindo <murial.anindo@lonett.com>
2023-06-04 15:49:32 -0400

    added some tests that are not working

--- project_app/components/test/header.test.js
+++ project_app/components/test/header.test.js
@@ -2,14 +2,24 @@ import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 import { HeaderButtons } from '../styled';
 
-describe('HeaderButtons component', () => {
-  it('should render Header component correctly', () => {
-    render(<Header />);
-    const element = screen.getByRole('Login');
+//test if login is rendered on button
+
+describe('HeaderButtons ', () => {
+  it('renders the Login on Header page', () => {
+    const { getByText } = render(<HeaderButtons />);
+    const element = screen.getByLabelText('Login');
     expect(element).toBeInTheDocument();
+    expect(getByText(element)).toBeInTheDocument();
   });
 });
 
-//test if login is rendered on button
+//test if Join Us! is rendered on button
 
-//test if signup is rendered on button
+describe('HeaderButtons ', () => {
+  it('renders the Login on Header page', () => {
+    const { getByText } = render(<HeaderButtons />);
+    const element = screen.getByLabelText('Join Us!');
+    expect(element).toBeInTheDocument();
+    expect(getByText(element)).toBeInTheDocument();
+  });
+});

d84bd46b9593bad01d97d9e3c939b28163dc5970
Murial Anindo <murial.anindo@lonett.com>
2023-06-18 18:26:32 -0400

    testing login button

--- project_app/components/test/header.test.js
+++ project_app/components/test/header.test.js
@@ -1,36 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import Header from '../Header';
-import Button from '../Header';
-import { HeaderButtons } from '../styled';
+import HeaderButtons, { validateInput } from '../styled';
 
-//test if login is rendered on button
-
-describe('HeaderButtons ', () => {
+describe('HeaderButtons', () => {
   it('renders the Login on Header page', () => {
-    const { getByText } = render(
-      <HeaderButtons>
-        <Button />
-      </HeaderButtons>
-    );
-    const element = screen.getByLabelText('Login');
-    expect(element).toBeInTheDocument();
-    expect(getByText(element)).toBeInTheDocument();
+    const buttonText = screen.getByLabelText('Login');
+    render(<HeaderButtons>{buttonText}</HeaderButtons>);
+    expect(buttonText).toBeInTheDocument();
   });
-});
-
-//test if Join Us! is rendered on button
-
-describe('HeaderButtons ', () => {
-  it('renders the Login on Header page', () => {
-    const { getByText } = render(<HeaderButtons />);
-    const element = screen.getByLabelText('Join Us!');
-    expect(element).toBeInTheDocument();
-    expect(getByText(element)).toBeInTheDocument();
-  });
-});
-
-test('renders the Login on the Header Page', () => {
-  render(<App />);
-  expect(screen.getByRole('button')).toHaveTextContent(/Doggy Directory/);
-  expect(screen.getByRole('combobox')).toHaveDisplayValue('Select a breed');
 });

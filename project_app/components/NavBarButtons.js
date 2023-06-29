6dc2ed1279972cbf2327e61041b624b548d9d1d6
Enrico Emil Dela Rosa <enricodelarosa01@gmail.com>
2023-06-29 14:35:36 +0800

    Link Reviews Button to reviews page and fix navbar buttons padding

--- project_app/components/NavBarButtons.js
+++ project_app/components/NavBarButtons.js
@@ -1,13 +1,16 @@
 import Button from '@mui/material/Button';
 import { NavbarButtonContainer } from './styled';
 import styled from "styled-components";
+import Link from 'next/link';
 
 function NavBarButtons() {
   return (
     <NavbarButtonContainer>
+        <Link href="/Reviews">
       <Button label="Events">
         <p>Reviews</p>
       </Button>
+      </Link>
       <Button label="FAQ">
         <p>Challenges</p>
       </Button>

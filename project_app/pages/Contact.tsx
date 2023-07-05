de186d7ecd667cce466191856a69a9d09639ef21
Murial Anindo <murial.anindo@lonett.com>
2023-07-05 18:11:59 -0400

    adding contact page

--- project_app/pages/Contact.tsx
+++ project_app/pages/Contact.tsx
@@ -1,5 +1,25 @@
-function contact() {
-  return <div>Contact</div>;
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { ProductContainer } from '../components/styled';
+import {
+  NavbarText,
+  AboutWrapperContainer,
+  AboutFooterContainer,
+  AboutHeaderWrapper
+} from './style';
+
+function Contact() {
+  return (
+    <AboutWrapperContainer>
+      <AboutHeaderWrapper>
+        <Header header={<NavbarText>Contact Us</NavbarText>} />
+      </AboutHeaderWrapper>
+      <AboutFooterContainer>
+        <ProductContainer />
+        <Footer />
+      </AboutFooterContainer>
+    </AboutWrapperContainer>
+  );
 }
 
-export default contact;
+export default Contact

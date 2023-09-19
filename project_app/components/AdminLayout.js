e069bab36cc60b64006ff339a4645e0935991e35
Murial Anindo <murial.anindo@lonett.com>
2023-09-19 15:36:56 -0400

    changed review

--- project_app/components/AdminLayout.js
+++ project_app/components/AdminLayout.js
@@ -1,30 +1,36 @@
-import React from "react";
-import Image from "next/image";
-import styled from "styled-components";
+import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
 import Head from 'next/head';
 
 export default function AdminLayout(props) {
-    return (
-        <>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet" />
-            </Head>
-            <AdminLayoutStyle>
-                <LogoContainer>
-                    <Image
-                        src="/logo.svg"
-                        width={227.77}
-                        height={150}
-                        alt="Ain't Board Logo"
-                    />
-                </LogoContainer>
-                <Content>{props.children}</Content>
-
-            </AdminLayoutStyle>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <AdminLayoutStyle>
+        <LogoContainer>
+          <Image
+            src="/logo.svg"
+            width={227.77}
+            height={150}
+            alt="Ain't Board Logo"
+          />
+        </LogoContainer>
+        {/* <Content>{props.children}</Content> */}
+      </AdminLayoutStyle>
+    </>
+  );
 }
 
 const ImageContainer = styled.div`
@@ -38,7 +44,7 @@ const ImageContainer = styled.div`
 const LogoContainer = styled.div`
   position: absolute;
   top: 0;
-`
+`;
 
 const Content = styled.div`
   flex-grow: 1;
@@ -53,5 +59,5 @@ const AdminLayoutStyle = styled.div`
   flex-direciton: column;
   height: 100%;
   width: 100%;
-background-color: #353535;
+  background-color: #353535;
 `;

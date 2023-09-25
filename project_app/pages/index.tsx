2507aeffed69571b388f8eb094f8ebd2adf83567
Murial Anindo <murial.anindo@lonett.com>
2023-09-25 09:16:01 -0400

    disabled reactstrict

--- project_app/pages/index.tsx
+++ project_app/pages/index.tsx
@@ -13,7 +13,8 @@ export default function Home(props:ToolBarProps) {
     <div>
       <Head>
       <ToolBar>{props.children} </ToolBar>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      </Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Josefin+Sans:ital,wght@1,200;1,700&family=Rubik:wght@500&display=swap" rel="stylesheet"/>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
@@ -24,18 +25,6 @@ export default function Home(props:ToolBarProps) {
       <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin ="anonymous"/>
       <link href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@700&family=Asap:ital,wght@0,300;1,400&display=swap" rel="stylesheet"/>
-      </Head>
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Josefin+Sans:ital,wght@1,200;1,700&family=Rubik:wght@500&display=swap" rel="stylesheet"/>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Josefin+Sans:ital,wght@1,200;1,700&family=Quicksand&family=Rubik:wght@500&display=swap" rel="stylesheet"/>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin ="anonymous"/>
-      <link href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@700&family=Asap:ital,wght@0,300;1,400&display=swap" rel="stylesheet"/> */}
       <Layout/>
     </div>
   );
